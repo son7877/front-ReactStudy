9f6ed2b2488e984496fbd73bb7cb4be446794012
son7877 <hongbeom1516@gmail.com>
2024-03-22 12:32:34 +0900

    jsStudy

--- day3/test3.js
+++ day3/test3.js
@@ -1,1 +1,8 @@
 const list = [1, 4, 5, 21, 42, 1, 3, 57, 9, 5231];
+//짝수 인지 아닌지 하나한 계산해서 리스트 만들어 보여주기
+const isEven = [];
+for (let i = 0; i < list.length; i++) {
+  const index = list[i] % 2 === 0 ? true : false;
+  isEven.push(index);
+}
+console.log(isEven);
