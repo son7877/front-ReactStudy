c965d631649bf534e31f3681ddd1c3d65abc2b04
son7877 <hongbeom1516@gmail.com>
2024-03-22 12:28:18 +0900

    jsStudy

--- day3/test2.js
+++ day3/test2.js
@@ -34,11 +34,28 @@ console.log(list3);
 const list4 = [];
 for (let num = 1; num <= 40; num++) {
   const index =
-    (num + "").includes("3") ||
-    (num + "").includes("6") ||
-    (num + "").includes("9")
+    `${num}`.includes("3") || `${num}`.includes("6") || `${num}`.includes("9")
       ? "짝"
       : num;
   list4.push(index);
 }
 console.log(list4);
+
+// 100~1000가 들어있는 리스트 중에서
+// 3,6,9만 들어있는 리스트를 만들자
+const mainlist = [];
+for (let i = 100; i <= 1000; i++) {
+  mainlist.push(i);
+}
+
+const list369 = [];
+for (let i = 0; i < mainlist.length; i++) {
+  const index =
+    `${mainlist[i]}`.includes("3") ||
+    `${mainlist[i]}`.includes("6") ||
+    `${mainlist[i]}`.includes("9")
+      ? mainlist[i]
+      : false;
+  if (index) list369.push(index);
+}
+console.log(list369);
