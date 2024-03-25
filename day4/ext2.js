85eb3ad69cddfa5e2750bff725d114d95c9140e0
son7877 <hongbeom7877@naver.com>
2024-03-25 12:01:10 +0900

    studyUpdate

--- day4/ext2.js
+++ day4/ext2.js
@@ -3,5 +3,48 @@ const classRoom = {
     //list
     { name: "김부자", age: 22 },
     { name: "조진호", age: 32 },
+    { name: "김세영", age: 15 },
+    { name: "정희석", age: 33 },
   ],
 };
+//성인인지 판단해서 리스트 만들기
+const isAdultList = [];
+for (let i = 0; i < classRoom.students.length; i++) {
+  const student = classRoom.students[i];
+  isAdultList.push({ ...student, isAdult: student.age > 19 });
+}
+console.log(isAdultList);
+
+const [b, jh] = classRoom.students;
+const { name, age } = jh;
+console.log(b, jh);
+console.log(name, age);
+
+for (let i = 0; i < classRoom.students.length; i++) {
+  classRoom.students[i].age++;
+}
+console.log(classRoom.students);
+
+const ageup = [];
+const ageup2 = [];
+//원본 값 변동을 막기 위한 방법 1. 다시 생성
+for (let i = 0; i < classRoom.students.length; i++) {
+  const student = classRoom.students[i];
+  const { age } = student;
+  const obj = {
+    ...student,
+    age: age + 1,
+  };
+  ageup.push(obj);
+}
+//원본 값 변동을 막기 위한 방법 2. spread 구문
+for (let i = 0; i < classRoom.students.length; i++) {
+  const obj2 = {
+    ...classRoom.students[i], // 변동하려는 값 이외는 ...처리를 통해 동결
+    age: classRoom.students[i].age + 1,
+  };
+  ageup2.push(obj2);
+}
+console.log(ageup);
+console.log(ageup2);
+console.log(classRoom.students);
---
+++ day4/ext3.js
@@ -0,0 +1,34 @@
+const students = [
+  { name: "김부자", age: 22 },
+  { name: "조진호", age: 32 },
+  { name: "김세영", age: 15 },
+  { name: "정희석", age: 33 },
+];
+//map
+const isAdultList = students.map((el, i, arr) => {
+  // el : element 리스트 내의 요소
+  return { ...el, isAdult: el.age > 19 };
+});
+// console.log(isAdultList);
+
+const ageupFunc = (el) => ({ ...el, age: el.age + 1 });
+
+const ageup = students.map(ageupFunc);
+
+// console.log(ageup);
+
+const adultList = students.filter((el, index, arr) => {
+  return el.age > 19;
+});
+console.log(adultList);
+
+//성이 김씨인 사람만 뽑기
+const kimList = students.filter((el) => {
+  return el.name.charAt(0) == "김";
+});
+console.log(kimList);
+
+// sort정렬
+const ageAsc = students.sort((a, b) => b.age - a.age);
+console.log(ageAsc);
+console.log(students);
